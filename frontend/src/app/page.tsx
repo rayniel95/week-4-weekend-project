cec634b43377a9c58bdc31ff86c421079f6e1187
rayniel95 <rainyel.ramos@gmail.com>
2023-08-20 12:47:47 -0400

    set navigation to contract voting page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,21 +1,30 @@
 'use client'
 
 import 'bootstrap/dist/css/bootstrap.css'
+import { useRouter } from 'next/router'
+import { FormEvent, FormEventHandler, useState } from 'react'
 
 import { Button, Form, FormControl, FormGroup, FormLabel } from "react-bootstrap"
 
 
 //TODO - add as feature to deploy the contracts
 export default function Home() {
+  const [contractAddress, setContractAddress] = useState('')
+  const router = useRouter()
+  
+  function navigate(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    router.push(`/contract/${contractAddress}`)
+  }
   return (
     <main>
       <div>
-        <Form>
+        <Form onSubmit={navigate}>
           <FormGroup>
             <FormLabel>
               Contract address:
             </FormLabel>
-            <FormControl type='text' />
+            <FormControl type='text' value={contractAddress} onChange={e => setContractAddress(e.target.value)} />
           </FormGroup>
           <Button className='mt-2' type='submit'>Connect</Button>
         </Form>

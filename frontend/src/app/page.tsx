831e8a35c5615caf439d2d5bd733ff2d75111b98
rayniel95 <rainyel.ramos@gmail.com>
2023-08-20 13:56:41 -0400

    add connection for both contracts

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,34 +1,51 @@
 'use client'
 
 import 'bootstrap/dist/css/bootstrap.css'
-import { useRouter } from 'next/router'
-import { FormEvent, FormEventHandler, useState } from 'react'
+import { useRouter } from 'next/navigation'
+import { FormEvent, useState } from 'react'
 
-import { Button, Form, FormControl, FormGroup, FormLabel } from "react-bootstrap"
+import { Button, Container, Form, FormControl, FormGroup, FormLabel, Row } from "react-bootstrap"
 
-
+//TODO - improve ux
 //TODO - add as feature to deploy the contracts
 export default function Home() {
   const [contractAddress, setContractAddress] = useState('')
   const router = useRouter()
-  
+
   function navigate(e: FormEvent<HTMLFormElement>) {
     e.preventDefault()
     router.push(`/contract/${contractAddress}`)
   }
   return (
     <main>
-      <div>
-        <Form onSubmit={navigate}>
-          <FormGroup>
-            <FormLabel>
-              Contract address:
-            </FormLabel>
-            <FormControl type='text' value={contractAddress} onChange={e => setContractAddress(e.target.value)} />
-          </FormGroup>
-          <Button className='mt-2' type='submit'>Connect</Button>
-        </Form>
-      </div>
+      <Container>
+        <Row>
+          <div>
+            <Form onSubmit={navigate}>
+              <FormGroup>
+                <FormLabel>
+                  ERC20Votes Contract address:
+                </FormLabel>
+                <FormControl type='text' value={contractAddress} onChange={e => setContractAddress(e.target.value)} />
+              </FormGroup>
+              <Button className='mt-2' type='submit'>Connect</Button>
+            </Form>
+          </div>
+        </Row>
+        <Row className='mt-2'>
+          <div>
+            <Form onSubmit={navigate}>
+              <FormGroup>
+                <FormLabel>
+                  TokenizedBallot Contract address:
+                </FormLabel>
+                <FormControl type='text' value={contractAddress} onChange={e => setContractAddress(e.target.value)} />
+              </FormGroup>
+              <Button className='mt-2' type='submit'>Connect</Button>
+            </Form>
+          </div>
+        </Row>
+      </Container>
     </main>
   )
 }

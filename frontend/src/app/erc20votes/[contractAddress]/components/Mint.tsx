ecbf6107a8ff71124c5ed70662ae4d739a822c45
rayniel95 <rainyel.ramos@gmail.com>
2023-08-21 09:05:19 -0400

    fix mint component

--- frontend/src/app/erc20votes/[contractAddress]/components/Mint.tsx
+++ frontend/src/app/erc20votes/[contractAddress]/components/Mint.tsx
@@ -4,10 +4,13 @@ import { useState, useEffect, FormEvent } from "react";
 import { Button, Container, Form, FormControl, FormGroup, FormLabel } from "react-bootstrap";
 import { useAccount, useContractWrite } from "wagmi";
 import MyERC20Votes from "../../../../../../blockchain/artifacts/contracts/MyERC20Votes.sol/MyERC20Votes.json";
+import { ethers } from "ethers";
 
 
 export default function Mint({ contractAddress }: { contractAddress: string }) {
     const [mintAmount, setMintAmount] = useState('0');
+    const [mintAddress, setMintAddress] = useState('');
+
     const { data, isLoading, isSuccess, write } = useContractWrite({
         //@ts-ignore
         address: contractAddress,
@@ -20,7 +23,7 @@ export default function Mint({ contractAddress }: { contractAddress: string }) {
     function mint(e: FormEvent<HTMLFormElement>) {
         e.preventDefault();
         write({
-            args: [parseInt(mintAmount)],
+            args: [mintAddress, ethers.parseUnits(mintAmount, parseInt(process.env.NEXT_PUBLIC_ERC20VOTES_TOKEN_DECIMALS!))],
             //@ts-ignore
             from: address,
             // value: parseEther('0.01'),
@@ -30,9 +33,12 @@ export default function Mint({ contractAddress }: { contractAddress: string }) {
     return (
         <>
             <Container>
+                <h5>Mint</h5>
                 <Form onSubmit={mint}>
                     <FormGroup>
-                        <FormLabel>Mint</FormLabel>
+                        <FormLabel>Address</FormLabel>
+                        <FormControl type='text' value={mintAddress} onChange={e => setMintAddress(e.target.value)} />
+                        <FormLabel>Amount</FormLabel>
                         <FormControl type='text' value={mintAmount} onChange={e => setMintAmount(e.target.value)} />
                     </FormGroup>
                     {isDisconnected && "Please connect your wallet"}
